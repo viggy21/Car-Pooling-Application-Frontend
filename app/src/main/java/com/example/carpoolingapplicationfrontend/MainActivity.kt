ce43d4d0982ea1f1bf998cf232f4b4419a62d54d
vickyhuang <vickyhuang025@gmail.com>
2026-04-11 21:10:34 +1000

    Initial commit

---
+++ app/src/main/java/com/example/carpoolingapplicationfrontend/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.carpoolingapplicationfrontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.carpoolingapplicationfrontend.ui.theme.CarPoolingApplicationFrontendTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CarPoolingApplicationFrontendTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CarPoolingApplicationFrontendTheme {
+        Greeting("Android")
+    }
+}
